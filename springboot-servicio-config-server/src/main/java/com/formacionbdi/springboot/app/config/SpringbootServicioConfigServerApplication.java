27f7da4e36125d25a320dc6ed8591d2188d02308
RostenRoss <costantini.nestor.m@gmail.com>
2022-03-16 10:24:58 -0300

    Se comentarion las configuraciones en los archivos de configuracion de los micros.

--- springboot-servicio-config-server/src/main/java/com/formacionbdi/springboot/app/config/SpringbootServicioConfigServerApplication.java
+++ springboot-servicio-config-server/src/main/java/com/formacionbdi/springboot/app/config/SpringbootServicioConfigServerApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
-@EnableConfigServer
+@EnableConfigServer //Para indicar que el micro servicio es un config server.
 @SpringBootApplication
 public class SpringbootServicioConfigServerApplication {
 
